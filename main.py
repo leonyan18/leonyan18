9af0aa583f4a699c137f5de70ffa34c336b86085
Leon Yan <33154558+leonyan18@users.noreply.github.com>
2022-12-29 00:03:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,16 +26,15 @@ def get_link_info(feed_url, num):
 
 def main():
     insert_info =  get_link_info("http://dawnchannel.tech/content.json", 3)
-    print(insert_info)
     # 替换 ---start--- 到 ---end--- 之间的内容
     # pytz.timezone('Asia/Shanghai')).strftime('%Y年%m月%d日%H时M分')
     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
-    insert_info = "## Recent blog posts(" + "update time:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') +")\n" + insert_info + "\n\n<br/>\n\n## 💻:keyboard: Languages and Tools "
+    insert_info = "## Recent Blog Posts(" + "update time:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S') +")\n" + insert_info + "\n\n<br/>\n\n## 💻:keyboard: Languages and Tools "
     # 获取README.md内容
-    
+    print(insert_info)
     with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
         readme_md_content = f.read()
-    new_readme_md_content = re.sub(r'## Recent blog posts(.|\n)*<br/>\n\n## 💻:keyboard: Languages and Tools ', insert_info, readme_md_content)
+    new_readme_md_content = re.sub(r'## Recent Blog Posts(.|\n)*<br/>\n\n## 💻:keyboard: Languages and Tools ', insert_info, readme_md_content)
     with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
         f.write(new_readme_md_content)
 main()
